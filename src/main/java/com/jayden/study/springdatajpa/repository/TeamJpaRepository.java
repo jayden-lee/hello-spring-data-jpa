5cbca50475ba9dea4280f42ce82a62516210ed65
jayden-lee <gwanggi.lee@gmail.com>
2020-02-16 00:09:45 +0900

    순수 JPA 기반 리포지토리 생성

---
+++ src/main/java/com/jayden/study/springdatajpa/repository/TeamJpaRepository.java
@@ -0,0 +1,41 @@
+package com.jayden.study.springdatajpa.repository;
+
+import com.jayden.study.springdatajpa.entity.Team;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery(
+            "select t from Team t", Team.class
+        ).getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        return Optional.ofNullable(em.find(Team.class, id));
+    }
+
+    public long count() {
+        return em.createQuery(
+            "select count(t) from Team t", Long.class
+        ).getSingleResult();
+    }
+}
